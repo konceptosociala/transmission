1a8572c22492f658ced4f1ab49fd052742e479f5
Oleksandr Hnutov <sanerhp.mail@gmail.com>
2025-10-24 13:15:24 +0300

    generate mesh

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,16 +1,17 @@
 {-# LANGUAGE PatternSynonyms #-}
 module Main where
 
-import Raylib.Util (withWindow, whileWindowOpen_, mode3D, drawing)
+import Raylib.Util (withWindow, whileWindowOpen_, mode3D, drawing, managed)
 import Raylib.Core (clearBackground, disableCursor, toggleFullscreen, getRenderWidth, getRenderHeight)
 import Raylib.Types (pattern Vector3, Color (Color))
-import Raylib.Core.Models (drawModelEx)
+import Raylib.Core.Models (drawModelEx, loadModelFromMesh)
 import Raylib.Util.Colors (white, black)
 import Raylib.Core.Text (drawFPS)
 import Control.Monad (when)
 import GameState (Scene(ScnMainMenu, ScnGame, ScnOptions), State (showFps, currentScene, camera), initState, updateState)
 import Utils (loadTexturedModel, drawButton, todo')
 import MainMenu (SceneMainMenu(SceneMainMenu), MainMenuItem (..))
+import Level (cube)
 
 title :: String
 title = "T.R.A.N.S.M.I.S.S.I.O.N"
@@ -25,6 +26,7 @@ main = do
       toggleFullscreen
 
       logo <- loadTexturedModel w "assets/logo.obj" "assets/logo.png"
+      cube <- managed w $ loadModelFromMesh =<< cube
 
       flip whileWindowOpen_ initState $
          \s -> do
@@ -34,7 +36,8 @@ main = do
                   ScnMainMenu (SceneMainMenu item rot) -> do
                      clearBackground black
                      mode3D (camera s) $ do
-                        drawModelEx logo (Vector3 0 2 0) (Vector3 0 1 0) rot 0.5 white
+                        -- drawModelEx logo (Vector3 0 2 0) (Vector3 0 1 0) rot 0.5 white
+                        drawModelEx cube (Vector3 0 1 0) (Vector3 1 1 1) rot 0.5 white
 
                      drawButton "New game" screenSize (-60) MmiNewGame item
                      drawButton "Options" screenSize 0 MmiOptions item
